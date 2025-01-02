0d914a9d144dade96e56e065a8e25db56d67fdd6
mehul <guptamehul642@gmail.com>
2025-01-02 16:49:20 +0530

    person updated to the list of person

--- src/main/java/com/example/tasks/GetPerson.java
+++ src/main/java/com/example/tasks/GetPerson.java
@@ -4,18 +4,29 @@ import com.common.Person;
 import org.platformlambda.core.annotations.PreLoad;
 import org.platformlambda.core.models.TypedLambdaFunction;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @PreLoad(route = "person.pojo", instances = 100, isPrivate = false)
-public class GetPerson implements TypedLambdaFunction<Map<String, Object>, Person> {
+public class GetPerson implements TypedLambdaFunction<Map<String,Object>, List<Person>> {
     @Override
-    public Person handleEvent(Map<String, String> headers, Map<String, Object> input, int instance) throws Exception {
+    public List<Person> handleEvent(Map<String, String> headers,Map<String,Object> input, int instance) throws Exception {
+
         Person person = new Person();
+        Person p1 = new Person();
         person.setId(1234);
         person.setName("Amey");
         person.setAddress("123 Address");
+
+        p1.setId(567);
+        p1.setName("Abhay");
+        p1.setAddress("567 Address");
+
         System.out.println(person.toString());
-        return person;
+
+        return Arrays.asList(person,p1);
     }
 
 
